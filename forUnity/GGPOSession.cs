750e5096294e3bf6e7a8391de4b7ea028a7c8bee
nicehero <85948597@qq.com>
2021-11-10 12:47:28 +0800

    forUnity

---
+++ forUnity/GGPOSession.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using Unity.Collections;
+
+namespace UnityGGPO {
+
+    // @TODO Move
+    public interface IPerfUpdate {
+
+        void ggpoutil_perfmon_update(GGPONetworkStats[] statss);
+    }
+
+    public static partial class GGPO {
+
+        public static class Session {
+            // Pass throughs
+
+            public delegate bool OnEventConnectedToPeerDelegate(int connected_player);
+
+            public delegate bool OnEventSynchronizingWithPeerDelegate(int synchronizing_player, int synchronizing_count, int synchronizing_total);
+
+            public delegate bool OnEventSynchronizedWithPeerDelegate(int synchronized_player);
+
+            public delegate bool OnEventRunningDelegate();
+
+            public delegate bool OnEventConnectionInterruptedDelegate(int connection_interrupted_player, int connection_interrupted_disconnect_timeout);
+
+            public delegate bool OnEventConnectionResumedDelegate(int connection_resumed_player);
+
+            public delegate bool OnEventDisconnectedFromPeerDelegate(int disconnected_player);
+
+            public delegate bool OnEventEventcodeTimesyncDelegate(int timesync_frames_ahead);
+
+            public delegate bool SafeLoadGameStateDelegate(NativeArray<byte> data);
+
+            public delegate bool SafeLogGameStateDelegate(string filename, NativeArray<byte> data);
+
+            public delegate bool SafeSaveGameStateDelegate(out NativeArray<byte> data, out int checksum, int frame);
+
+            public delegate void SafeFreeBufferDelegate(NativeArray<byte> data);
+
+            private static readonly Dictionary<long, NativeArray<byte>> cache = new Dictionary<long, NativeArray<byte>>();
+
+            private static BeginGameDelegate beginGameCallback;
+            private static AdvanceFrameDelegate advanceFrameCallback;
+
+            private static SafeLoadGameStateDelegate loadGameStateCallback;
+            private static SafeLogGameStateDelegate logGameStateCallback;
+            private static SafeSaveGameStateDelegate saveGameStateCallback;
+            private static SafeFreeBufferDelegate freeBufferCallback;
+
+            private static OnEventConnectedToPeerDelegate onEventConnectedToPeer;
+            private static OnEventSynchronizingWithPeerDelegate onEventSynchronizingWithPeer;
+            private static OnEventSynchronizedWithPeerDelegate onEventSynchronizedWithPeer;
+            private static OnEventRunningDelegate onEventRunning;
+            private static OnEventConnectionInterruptedDelegate onEventConnectionInterrupted;
+            private static OnEventConnectionResumedDelegate onEventConnectionResumed;
+            private static OnEventDisconnectedFromPeerDelegate onEventDisconnectedFromPeer;
+            private static OnEventEventcodeTimesyncDelegate onEventTimesync;
+
+            private static IntPtr _beginGameCallback;
+            private static IntPtr _advanceFrameCallback;
+            private static IntPtr _loadGameStateCallback;
+            private static IntPtr _logGameStateCallback;
+            private static IntPtr _saveGameStateCallback;
+            private static IntPtr _freeBufferCallback;
+            private static IntPtr _onEventCallback;
+
+            public static void Init(LogDelegate log) {
+                GGPO.SetLogDelegate(log);
+                unity_ggpo_start();
+            }
+
+            public static int StartCSSession(string playerID
+            , string serverIP
+            , int serverPort
+            , string room
+            , int num_players
+            , int num_spectators
+            ,BeginGameDelegate beginGame,
+            AdvanceFrameDelegate advanceFrame,
+            SafeLoadGameStateDelegate loadGameState,
+            SafeLogGameStateDelegate logGameState,
+            SafeSaveGameStateDelegate saveGameState,
+            SafeFreeBufferDelegate freeBuffer,
+            OnEventConnectedToPeerDelegate onEventConnectedToPeer,
+            OnEventSynchronizingWithPeerDelegate onEventSynchronizingWithPeer,
+            OnEventSynchronizedWithPeerDelegate onEventSynchronizedWithPeer,
+            OnEventRunningDelegate onEventRunning,
+            OnEventConnectionInterruptedDelegate onEventConnectionInterrupted,
+            OnEventConnectionResumedDelegate onEventConnectionResumed,
+            OnEventDisconnectedFromPeerDelegate onEventDisconnectedFromPeer,
+            OnEventEventcodeTimesyncDelegate onEventTimesync)
+            {
+                beginGameCallback = beginGame;
+                advanceFrameCallback = advanceFrame;
+                loadGameStateCallback = loadGameState;
+                logGameStateCallback = logGameState;
+                saveGameStateCallback = saveGameState;
+                freeBufferCallback = freeBuffer;
+
+                Session.onEventConnectedToPeer = onEventConnectedToPeer;
+                Session.onEventSynchronizingWithPeer = onEventSynchronizingWithPeer;
+                Session.onEventSynchronizedWithPeer = onEventSynchronizedWithPeer;
+                Session.onEventRunning = onEventRunning;
+                Session.onEventConnectionInterrupted = onEventConnectionInterrupted;
+                Session.onEventConnectionResumed = onEventConnectionResumed;
+                Session.onEventDisconnectedFromPeer = onEventDisconnectedFromPeer;
+                Session.onEventTimesync = onEventTimesync;
+
+                unsafe
+                {
+                    _beginGameCallback = Marshal.GetFunctionPointerForDelegate<BeginGameDelegate>(OnBeginGame);
+                    _advanceFrameCallback = Marshal.GetFunctionPointerForDelegate<AdvanceFrameDelegate>(OnAdvanceFrame);
+                    _loadGameStateCallback = Marshal.GetFunctionPointerForDelegate<LoadGameStateDelegate>(LoadGameState);
+                    _logGameStateCallback = Marshal.GetFunctionPointerForDelegate<LogGameStateDelegate>(LogGameState);
+                    _saveGameStateCallback = Marshal.GetFunctionPointerForDelegate<SaveGameStateDelegate>(SaveGameState);
+                    _freeBufferCallback = Marshal.GetFunctionPointerForDelegate<FreeBufferDelegate>(FreeBuffer);
+                    _onEventCallback = Marshal.GetFunctionPointerForDelegate<OnEventDelegate>(OnEvent);
+                }
+                var result = GGPO.unity_ggpo_game_init(
+                    playerID, serverIP, serverPort,room, num_players, num_spectators,
+                    _beginGameCallback, _advanceFrameCallback, _loadGameStateCallback,
+                    _logGameStateCallback, _saveGameStateCallback, _freeBufferCallback, _onEventCallback);
+
+                return result;
+            }
+
+
+            public static int GetNetworkStats(int phandle, out GGPONetworkStats stats) {
+                stats = new GGPONetworkStats();
+                var result = GGPO.GetNetworkStats(phandle,
+                    out stats.send_queue_len,
+                    out stats.recv_queue_len,
+                    out stats.ping,
+                    out stats.kbps_sent,
+                    out stats.local_frames_behind,
+                    out stats.remote_frames_behind
+                );
+                return result;
+            }
+
+            public static int SetDisconnectNotifyStart(int timeout) {
+                var result = GGPO.SetDisconnectNotifyStart(timeout);
+                return result;
+            }
+
+            public static int SetDisconnectTimeout(int timeout) {
+                var result = GGPO.SetDisconnectTimeout(timeout);
+                return result;
+            }
+
+            public static long[] SynchronizeInput(int length, out int disconnect_flags) {
+                return GGPO.SynchronizeInput(length, out disconnect_flags);
+            }
+
+            public static int AddLocalInput(int local_player_handle, long inputs) {
+                var result = GGPO.AddLocalInput(local_player_handle, inputs);
+                return result;
+            }
+
+            public static int CloseSession() {
+                foreach (var data in cache.Values) {
+                    freeBufferCallback(data);
+                }
+                cache.Clear();
+                var result = GGPO.CloseSession();
+                return result;
+            }
+
+            public static int Idle(int time) {
+                var result = GGPO.Idle(time);
+                return result;
+            }
+
+            public static int DisconnectPlayer(int phandle) {
+                var result = GGPO.DisconnectPlayer(phandle);
+                return result;
+            }
+
+            public static int SetFrameDelay(int phandle, int frame_delay) {
+                var result = GGPO.SetFrameDelay(phandle, frame_delay);
+                return result;
+            }
+
+            public static int AdvanceFrame() {
+                var result = GGPO.AdvanceFrame();
+                return result;
+            }
+
+            public static void Log(string v) {
+                GGPO.Log(v);
+            }
+
+            public static void OnDispose() {
+                CloseSession();
+                unity_ggpo_stop();
+            }
+
+            // Callbacks
+
+            private static unsafe void FreeBuffer(void* dataPtr) {
+                if (cache.TryGetValue((long)dataPtr, out var data)) {
+                    freeBufferCallback(data);
+                    cache.Remove((long)dataPtr);
+                }
+            }
+
+            private static unsafe bool SaveGameState(void** buffer, int* outLen, int* outChecksum, int frame) {
+                var result = saveGameStateCallback(out var data, out int checksum, frame);
+                var ptr = Utils.ToPtr(data);
+                cache[(long)ptr] = data;
+
+                *buffer = ptr;
+                *outLen = data.Length;
+                *outChecksum = checksum;
+                return result;
+            }
+
+            private static unsafe bool LogGameState(string filename, void* buffer, int length) {
+                return logGameStateCallback(filename, Utils.ToArray(buffer, length));
+            }
+
+            private static unsafe bool LoadGameState(void* buffer, int length) {
+                return loadGameStateCallback(Utils.ToArray(buffer, length));
+            }
+
+            private static bool OnAdvanceFrame(int flags) {
+                return advanceFrameCallback.Invoke(flags);
+            }
+
+            private static bool OnBeginGame(string name) {
+                return beginGameCallback.Invoke(name);
+            }
+
+            private static bool OnEvent(IntPtr evtPtr) {
+                /*
+                code = data[0];
+                connected.player = data[1];
+                synchronizing.player = data[1];
+                synchronizing.count = data[2];
+                synchronizing.total = data[3];
+                synchronized.player = data[1];
+                disconnected.player = data[1]
+                timesync.frames_ahead = data[1];
+                connection_interrupted.player = data[1];
+                connection_interrupted.disconnect_timeout = data[2];
+                connection_resumed.player = data[1];
+                */
+
+                int[] data = new int[4];
+                Marshal.Copy(evtPtr, data, 0, 4);
+                switch (data[0]) {
+                    case EVENTCODE_CONNECTED_TO_PEER:
+                        return onEventConnectedToPeer(data[1]);
+
+                    case EVENTCODE_SYNCHRONIZING_WITH_PEER:
+                        return onEventSynchronizingWithPeer(data[1], data[2], data[3]);
+
+                    case EVENTCODE_SYNCHRONIZED_WITH_PEER:
+                        return onEventSynchronizedWithPeer(data[1]);
+
+                    case EVENTCODE_RUNNING:
+                        return onEventRunning();
+
+                    case EVENTCODE_DISCONNECTED_FROM_PEER:
+                        return onEventDisconnectedFromPeer(data[1]);
+
+                    case EVENTCODE_TIMESYNC:
+                        return onEventTimesync(data[1]);
+
+                    case EVENTCODE_CONNECTION_INTERRUPTED:
+                        return onEventConnectionInterrupted(data[1], data[2]);
+
+                    case EVENTCODE_CONNECTION_RESUMED:
+                        return onEventConnectionResumed(data[1]);
+                }
+                return false;
+            }
+        }
+    }
+}
--- lib/ggpo/ggpo.cpp
+++ lib/ggpo/ggpo.cpp
@@ -306,3 +306,210 @@ ggpo_start_cssession(GGPOSession **session,
 	   return e.ggpoError;
 	}
 }
+
+#include <nicenet/nicenet.h>
+#ifdef _WIN32
+#include <windows.h>
+#endif
+extern "C"{
+	GGPOSession* g_ggpo = nullptr;
+	std::string g_playerID;
+	void unity_ggpo_start(){
+		nicehero::start(true);
+	}
+	void unity_ggpo_stop(){
+		if (g_ggpo)
+		{
+			ggpo_close_session(g_ggpo);
+			g_ggpo = nullptr;
+			g_playerID = "";
+		}
+		nicehero::stop();
+#ifdef _WIN32
+		Sleep(1000);
+#else
+		sleep(1);
+#endif
+		nicehero::joinMain();
+	}
+	int UggSetDisconnectNotifyStart(int timeout)
+	{
+		return ggpo_set_disconnect_notify_start(g_ggpo, timeout);
+	}
+
+	int UggSetDisconnectTimeout(int timeout)
+	{
+		return ggpo_set_disconnect_timeout(g_ggpo, timeout);
+	}
+
+	int UggSynchronizeInput(uint64_t* inputs, int length, int& disconnect_flags)
+	{
+		return ggpo_synchronize_input(g_ggpo, inputs, sizeof(uint64_t) * length, &disconnect_flags);
+	}
+
+	int UggAddLocalInput(int local_player_handle, uint64_t input)
+	{
+		return ggpo_add_local_input(g_ggpo, local_player_handle, &input, sizeof(uint64_t));
+	}
+
+	int UggCloseSession()
+	{
+		if (g_ggpo)
+		{
+			ggpo_close_session(g_ggpo);
+			g_ggpo = nullptr;
+			g_playerID = "";
+		}
+		return 0;
+	}
+
+	int UggIdle(int timeout)
+	{
+		return ggpo_idle(g_ggpo, timeout);
+	}
+
+	typedef void (*LogDelegate)(const char* text);
+	typedef bool (*BeginGameDelegate)(const char* text);
+	typedef bool (*AdvanceFrameDelegate)(int flags);
+	typedef bool (*LoadGameStateDelegate)(unsigned char* buffer, int length);
+	typedef bool (*LogGameStateDelegate)(char* text, unsigned char* buffer, int length);
+	typedef bool (*SaveGameStateDelegate)(unsigned char** buffer, int* len, int* checksum, int frame);
+	typedef void (*FreeBufferDelegate)(void* buffer);
+	typedef bool (*OnEventDelegate)(GGPOEvent* info);
+	
+
+	int unity_ggpo_game_init(
+	const char* playerID
+	, const char* serverIP, int serverPort
+	, const char* room
+	, int num_players, int num_spectators,    
+	BeginGameDelegate beginGame,
+    AdvanceFrameDelegate advanceFrame,
+    LoadGameStateDelegate loadGameState,
+    LogGameStateDelegate logGameState,
+    SaveGameStateDelegate saveGameState,
+    FreeBufferDelegate freeBuffer,
+    OnEventDelegate onEvent)
+	{
+	   GGPOErrorCode result;
+
+	   GGPOSessionCallbacks cb = { 0 };
+	   cb.begin_game      = beginGame;
+	   cb.advance_frame	 = advanceFrame;
+	   cb.load_game_state = loadGameState;
+	   cb.save_game_state = saveGameState;
+	   cb.free_buffer     = freeBuffer;
+	   cb.on_event        = onEvent;
+	   cb.log_game_state  = logGameState;
+
+	   result = ggpo_start_cssession(&g_ggpo, &cb, "vectorwar", num_players, sizeof(int),room,playerID,serverIP,(unsigned short)serverPort);
+	   if (result == GGPO_ERRORCODE_CS_CONNECT_FAILED)
+	   {
+		   int retryConnect = 0;
+			while (ggpo_start_cssession(
+				&g_ggpo, &cb, "vectorwar", num_players, sizeof(int), room, playerID, serverIP, (unsigned short)serverPort
+			) == GGPO_ERRORCODE_CS_CONNECT_FAILED) 
+			{
+				if (retryConnect > 4)
+				{
+					return result;
+				}
+#ifdef _WIN32
+				Sleep(100);
+#else
+				usleep(100000);
+#endif
+				++retryConnect;
+			}
+	   }
+	   // automatically disconnect clients after 3000 ms and start our count-down timer
+	   // for disconnects after 1000 ms.   To completely disable disconnects, simply use
+	   // a value of 0 for ggpo_set_disconnect_timeout.
+	   ggpo_set_disconnect_timeout(g_ggpo, 3000);
+	   ggpo_set_disconnect_notify_start(g_ggpo, 1000);
+	   g_playerID = playerID;
+/*
+	   int i;
+	   for (i = 0; i < num_players + num_spectators; i++) {
+		  GGPOPlayerHandle handle;
+		  result = ggpo_add_player(g_ggpo, players + i, &handle);
+		  if (result != 0) {
+			  return result;
+		  }
+		  ngs.players[i].handle = handle;
+		  ngs.players[i].type = players[i].type;
+		  if (players[i].type == GGPO_PLAYERTYPE_LOCAL) {
+			 ngs.players[i].connect_progress = 100;
+			 ngs.local_player_handle = handle;
+			 ngs.SetConnectState(handle, Connecting);
+			 ggpo_set_frame_delay(g_ggpo, handle, FRAME_DELAY);
+		  } else {
+			 ngs.players[i].connect_progress = 0;
+		  }
+	   }
+*/
+	   return result;
+	}
+	int unity_ggpo_game_add_player(
+		int player_type,
+		int player_num,
+		const char* playerID,
+		int& phandle)
+	{
+		if (!g_ggpo){
+			return -1;
+		}
+		GGPOPlayer player;
+		player.size = sizeof(GGPOPlayer);
+		player.type = (GGPOPlayerType)player_type;
+		player.player_num = player_num;
+		strcpy(player.u.csremote.playerID, playerID);
+		int r = ggpo_add_player(g_ggpo, &player, &phandle);
+		if (r != 0) {
+			return r;
+		}
+		if (g_playerID == playerID){
+			ggpo_set_frame_delay(g_ggpo, phandle, 2);
+		}
+		return r;
+	}
+	int UggDisconnectPlayer(int phandle)
+	{
+		return ggpo_disconnect_player(g_ggpo, phandle);
+	}
+
+	int UggSetFrameDelay(int phandle, int frame_delay)
+	{
+		return ggpo_set_frame_delay(g_ggpo, phandle, frame_delay);
+	}
+
+	int UggAdvanceFrame()
+	{
+		return ggpo_advance_frame(g_ggpo);
+	}
+
+	void UggLog(const char* text)
+	{
+		ggpo_log(g_ggpo, text);
+	}
+
+	int UggGetNetworkStats(int phandle,
+		int& send_queue_len,
+		int& recv_queue_len,
+		int& ping,
+		int& kbps_sent,
+		int& local_frames_behind,
+		int& remote_frames_behind)
+	{
+		GGPONetworkStats stats;
+		auto result = ggpo_get_network_stats(g_ggpo, phandle, &stats);
+		send_queue_len = stats.network.send_queue_len;
+		recv_queue_len = stats.network.recv_queue_len;
+		ping = stats.network.ping;
+		kbps_sent = stats.network.kbps_sent;
+		local_frames_behind = stats.timesync.local_frames_behind;
+		remote_frames_behind = stats.timesync.remote_frames_behind;
+		return result;
+	}
+}
+
